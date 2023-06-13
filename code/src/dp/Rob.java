cb2cfcc1f3f2bae2c534f1a6ddde397920a2ab7d
lbl <libl@qixiangyun.com>
2023-06-13 18:42:55 +0800

    打家劫舍

---
+++ code/src/dp/Rob.java
@@ -0,0 +1,14 @@
+package dp;
+
+public class Rob {
+    public static void main(String[] args) {
+        int [] nums =new int[]{1,2,3,1};
+        Rob r = new Rob();
+        System.out.println(r.rob(nums));
+        
+    }
+    public int rob(int[] nums) {
+
+        return 1;
+    }
+}
