5303a58c6554c008e49b5675b2f8247a8f01d9f8
李炳乐 <2523890936@qq.com>
2023-06-14 23:25:01 +0800

    接雨水

---
+++ code/src/Math/NumberOne.java
@@ -0,0 +1,13 @@
+package Math;
+
+public class NumberOne {
+    public static void main(String[] args) {
+        NumberOne numberOne = new NumberOne();
+        System.out.println(numberOne.countDigitOne(13));
+    }
+    public int countDigitOne(int n) {
+
+
+        return 1;
+    }
+}
---
+++ code/src/dp/Rain.java
@@ -0,0 +1,14 @@
+package dp;
+
+public class Rain {
+    public static void main(String[] args) {
+        Rain rain = new Rain();
+        int []height = new int[]{0,1,0,2,1,0,1,3,2,1,2,1};
+        System.out.println(rain.trap(height));
+    }
+    public int trap(int[] height) {
+
+
+        return 1;
+    }
+}
