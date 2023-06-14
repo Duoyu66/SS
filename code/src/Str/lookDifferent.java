f182fe343c19b222625a5c5393798da1f0576e02
lbl <libl@qixiangyun.com>
2023-06-14 10:20:35 +0800

    找不同

---
+++ code/src/Str/lookDifferent.java
@@ -0,0 +1,33 @@
+package Str;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+* 找不同
+* */
+public class lookDifferent {
+    public static void main(String[] args) {
+
+        lookDifferent lookdifferent = new lookDifferent();
+        String str = "abcd";
+        String str2="abcde";
+
+
+        System.out.println(lookdifferent.findTheDifference(str,str2));
+    }
+    public char findTheDifference(String s, String t) {
+        char []chS = s.toCharArray();
+        char []chT = t.toCharArray();
+        ArrayList<Character> list = new ArrayList<>();
+        for (int i = 0; i < chT.length; i++) {
+            list.add(chT[i]);
+        }
+        for (int i = 0; i < chS.length; i++) {
+            if (list.contains(chS[i])){
+                list.remove(list.indexOf(chS[i]));
+            }
+        }
+        return list.get(0);
+    }
+}
